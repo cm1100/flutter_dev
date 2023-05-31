777e23235888783e5d239640146ae7f42a20c0d5
chaitanya malik <chaitanyamalik@chaitanyas-MacBook-Pro.local>
2023-05-31 11:29:57 +0530

    expense_tracker

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,8 +1,37 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 195, 61, 188));
+
 void main() {
-  return runApp(const MaterialApp(
-    home: Expenses(),
+  return runApp(MaterialApp(
+    theme: ThemeData().copyWith(
+      useMaterial3: true,
+      colorScheme: kColorScheme,
+      appBarTheme: AppBarTheme().copyWith(
+        backgroundColor: kColorScheme.onPrimaryContainer,
+        foregroundColor: kColorScheme.primaryContainer,
+      ),
+      cardTheme: CardTheme().copyWith(
+        color: kColorScheme.secondaryContainer,
+        margin: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 8,
+        ),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer),
+      ),
+      textTheme: ThemeData().textTheme.copyWith(
+            titleLarge: TextStyle(
+              fontWeight: FontWeight.normal,
+              color: kColorScheme.onSecondaryContainer,
+              fontSize: 16,
+            ),
+          ),
+    ),
+    home: const Expenses(),
   ));
 }
