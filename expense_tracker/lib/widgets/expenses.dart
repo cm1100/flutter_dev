28a675bf83f5af650edd46ad96ea1d2a43307446
chaitanya malik <chaitanyamalik@chaitanyas-MacBook-Pro.local>
2023-06-01 13:39:28 +0530

    flut

--- expense_tracker/lib/widgets/expenses.dart
+++ expense_tracker/lib/widgets/expenses.dart
@@ -1,3 +1,5 @@
+import 'package:expense_tracker/widgets/chart/chart.dart';
+import 'package:expense_tracker/widgets/chart/chart_bar.dart';
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/new_expense.dart';
@@ -89,6 +91,7 @@ class _ExpensesState extends State<Expenses> {
       ),
       body: Column(
         children: [
+          Chart(expenses: _registeredExpenses),
           Expanded(
             child: namedContent,
           )
